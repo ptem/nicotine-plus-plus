eff4053235c60377fd92467ee32762d3405bf934
pachiclana <javiermg1986@gmail.com>
2024-08-30 22:17:25 +0200

    code refactor. Added class with web api models

--- pynicotine/webapi.py
+++ pynicotine/webapi.py
@@ -1,6 +1,3 @@
-
-
-
 from pynicotine import slskmessages
 from pynicotine.events import events
 from pynicotine.config import config
@@ -10,67 +7,16 @@ from pynicotine.slskmessages import FileListMessage
 from pynicotine.core import core
 from pynicotine.logfacility import log
 from pynicotine.transfers import TransferStatus
+from pynicotine.webapi_models import WebApiSearchResult, FileDownloadedNotification, WebApiSearchModel, FileToDownload, TransferModel
 
-from pydantic import BaseModel
-from threading import Thread, excepthook
+from threading import Thread
 import pathlib
 from difflib import SequenceMatcher
 import requests
-from typing import Optional
 from fastapi import FastAPI
 import uvicorn
 import time
 import asyncio
-
-class WebApiSearchResult(BaseModel):
-    user: str
-    ip_address: str
-    port: int
-    has_free_slots: bool
-    inqueue: int
-    ulspeed: int
-    file_name: str
-    file_extension: str
-    file_path: str
-    file_size: int
-    file_h_length: str
-    bitrate: int
-    search_similarity: float
-    file_attributes: Optional[dict] = None
-
-class FileDownloadedNotification(BaseModel):
-    user: str
-    virtual_file_path: str
-    file_download_path: str
-
-class WebApiSearchModel(BaseModel):
-    search_term: str
-    wait_for_seconds: int
-    search_filters: Optional[dict] = None
-    smart_filters: Optional[bool] = None
-    
-
-class FileToDownload(BaseModel):
-    file_owner: str
-    file_virtual_path: str
-    file_size: int
-    file_attributes: Optional[dict] = None
-
-class TransferModel(BaseModel):
-    username: str
-    virtual_path: str
-    download_path: str
-    status: str
-    size: int
-    current_byte_offset: Optional[int] = None
-    download_percentage: Optional[str] = None
-    file_attributes: Optional[dict] = None
-
-class SearchReqResponseModel(BaseModel):
-    pass
-
-class SearchResultModel(BaseModel):
-    pass
 
 class AsyncUvicorn:
 
